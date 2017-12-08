265f0429497a456a4da667274992b9d98572a221
Blyuvit <julia_ivakina@yahoo.com>
2017-12-08 23:33:41 +0300

    Полнотекстовый поиск

--- templates/_pagination.php
+++ templates/_pagination.php
@@ -2,7 +2,8 @@
 	<ul class="pagination-list">	
 		<li class="pagination-item pagination-item-prev"><a>Назад</a></li>
 		<?php foreach ($pages as $value): ?>
-			<li class="pagination-item <?php if ($value == $page) echo 'pagination-item-active'; ?>"><a href="/?page=<?=$value;?>"><?=$value ?></a></li>
+		<?php $link = isset($search) ? "search.php?search=$search&find=Найти&page=$value" : "?page=$value"; ?>
+		<li class="pagination-item <?php if ($value == $page) echo 'pagination-item-active'; ?>"><a href="/<?=$link ?>"><?=$value ?></a></li>
 		<?php endforeach; ?>
 		<li class="pagination-item pagination-item-next"><a>Вперед</a></li>		
 	</ul>
